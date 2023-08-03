9ff3c1c03f3e181b3904ed51b1165e58c43a999d
RaulRivera <rirpgamer.12@hotmail.com>
2023-08-03 13:30:50 -0700

    new video

--- src/components/Videosr.js
+++ src/components/Videosr.js
@@ -3,8 +3,10 @@ import styles from "../styles/BigText.module.css"
 function Videosr() {
   return (
     <div className={styles.videoco}>
-      <video width="100%" height="100%" autoPlay muted playsInline loop src="/showreel.mp4" type="video/mp4" >
-      <h1>ready to start?</h1>
+      <video width="100%" height="100%" controls autoPlay muted playsInline loop >
+      <source src="/showreel.mp4" type="video/mp4"/>
+      <iframe width="100%" height="100%" src="https://youtu.be/zawS019QfKg" frameborder="0" allow=" autoplay;" ></iframe>
+      <h1>ready to star?</h1>
     </video>
     </div>
   )
