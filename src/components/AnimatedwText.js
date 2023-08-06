390742d4521ba1c283c8b942ce02b15a554b0684
RaulRivera <rirpgamer.12@hotmail.com>
2023-08-06 12:58:18 -0700

    try

--- src/components/AnimatedwText.js
+++ src/components/AnimatedwText.js
@@ -1,77 +1,11 @@
-"use client";
-import React, { useState, useEffect } from 'react';
-import styles from "../styles/BigText.module.css";
-
-const AnimatedwText = ({ text }) => {
-  const [currentFont, setCurrentFont] = useState(0);
-  const [randomChars, setRandomChars] = useState([]);
-  const [showRandomChars, setShowRandomChars] = useState(true);
-
-  const fonts = ['Eudoxus', 'Georgia', 'Arial', 'Helvetica', 'Verdana', 'Gt-America-EL', 'Times New Roman', 'Gt-America-EB'];
-  const specialCharacters = "!:[{}]^*()+-+/?";
-
-  const getRandomChars = () => {
-    let chars = [];
-    for (let i = 0; i < text.length; i++) {
-      if (Math.random() < 0.5) {
-        chars.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
-      } else {
-        chars.push(text[i]);
-      }
-    }
-    return chars;
-  };
-
-  const toggleRandomChars = () => {
-    setShowRandomChars((prevShowRandomChars) => !prevShowRandomChars);
-  };
-
-  useEffect(() => {
-    const keepKisu = () => {
-      setTimeout(() => {
-        toggleRandomChars();
-      }, 100);
-    };
-
-    if (text === "kisu") {
-      keepKisu();
-    }
-  }, [text]);
-
-  useEffect(() => {
-    const changeFontAndChars = () => {
-      setCurrentFont((prevFont) => (prevFont + 1) % fonts.length);
-      setRandomChars(getRandomChars());
-      toggleRandomChars();
-    };
-
-    const interval = setInterval(changeFontAndChars, 400);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [fonts]);
-
+import React from 'react'
+import styles from "../styles/BigText.module.css"
+function AnimatedwText() {
   return (
-    <div
-      className={styles.textmd}
-      style={{
-        transform: showRandomChars ? 'scale(1)' : 'scale(1.2)',
-        transformOrigin: 'center',
-      }}
-    >
-      <h1 className={styles.wordWrap}>
-        {text.split('').map((letter, index) => (
-          <span key={index} style={{
-            fontFamily: fonts[currentFont],
-            color: 'black',
-          }}>
-            {showRandomChars ? randomChars[index] : letter}
-          </span>
-        ))}
-      </h1>
-    </div>
-  );
-};
+    <h1 className={styles.textmd}>
+      KISU
+    </h1>
+  )
+}
 
-export default React.memo(AnimatedwText);
+export default AnimatedwText
